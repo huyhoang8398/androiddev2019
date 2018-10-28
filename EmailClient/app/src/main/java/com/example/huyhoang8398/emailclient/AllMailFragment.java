d8c3cee775e29072b11a6f93dc7377133e091f49
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-28 22:16:15 +0700

    Creating viewpager inside all mail fragment

--- EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/AllMailFragment.java
+++ EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/AllMailFragment.java
@@ -4,11 +4,44 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toolbar;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class AllMailFragment extends Fragment {
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Social", "Primary", "Promotions" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0: return new FragmentSocial();
+                case 1: return new FragmentPrimary();
+                case 2: return new FragmentPromotion();
+            }
+            return new FragmentPrimary(); // failsafe
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
