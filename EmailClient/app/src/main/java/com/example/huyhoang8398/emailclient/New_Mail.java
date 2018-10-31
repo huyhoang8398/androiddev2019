fd1ecd2bc571fa6bf6df391ebbf1acc05cea3db0
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-31 15:14:51 +0700

    added back button ( go to previous activity ) in action bar

--- EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/New_Mail.java
+++ EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/New_Mail.java
@@ -2,6 +2,7 @@ package com.example.huyhoang8398.emailclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class New_Mail extends AppCompatActivity {
     public New_Mail(){
@@ -12,5 +13,21 @@ public class New_Mail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_mail);
+
+        /// add back button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    /// add back button
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
