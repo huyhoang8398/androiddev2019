23ad30d7fea34f325579bbd8b6c5ba81a264e6cb
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-31 16:07:05 +0700

    better new mail edit text

--- EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/FragmentPrimary.java
+++ EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/FragmentPrimary.java
@@ -26,6 +26,7 @@ public class FragmentPrimary extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
             View view = inflater.inflate(R.layout.fragment_primary, container, false);
             return view;
         }
