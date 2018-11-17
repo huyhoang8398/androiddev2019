013ced5fbe194aa18282b70143730668ff3e58e8
huyhoang8398 <huyhoang8398@gmail.com>
2018-11-17 15:49:02 +0700

    updated refresh button

--- androiddev2019.bak/EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/activity/MainActivity.java
+++ androiddev2019.bak/EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -111,6 +112,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         searchView.setMenuItem(item);
 
         return true;
+    }
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_refesh:
+                finish();
+                startActivity(getIntent());
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 
     @Override
--- androiddev2019.bak/EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/fragments/PrimaryFragment.java
+++ androiddev2019.bak/EmailClient/app/src/main/java/com/example/huyhoang8398/emailclient/fragments/PrimaryFragment.java
@@ -1,9 +1,19 @@
 package com.example.huyhoang8398.emailclient.fragments;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.huyhoang8398.emailclient.R;
 
 
 public class PrimaryFragment extends BaseFragment {
@@ -14,11 +24,16 @@ public class PrimaryFragment extends BaseFragment {
     // Inflate the view for the fragment based on layout XML
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = super.onCreateView(inflater, container, savedInstanceState);
         String category = "category:primary";
         api.execute(category);
         return view;
     }
+
+
+
+
 }
 
 
